22520c2186b3b9fb29626ba1e4895996eccec77f
Kamil Kowalewski <szamil24@gmail.com>
2021-01-10 14:12:34 +0100

    Add repository layer in Core

---
+++ EventoCore/Repositories/IEventRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using EventoCore.Domain;
+
+namespace EventoCore.Repositories {
+
+    public interface IEventRepository {
+
+        Task<Event> GetByIdAsync(Guid id);
+
+        Task<Event> GetByNameAsync(string name);
+
+        Task<IEnumerable<Event>> SearchByNameAsync(string name = "");
+
+        Task<Event> AddAsync(Event @event);
+
+        Task<Event> UpdateAsync(Event @event);
+
+        Task<Event> DeleteAsync(Event @event);
+
+    }
+
+}
