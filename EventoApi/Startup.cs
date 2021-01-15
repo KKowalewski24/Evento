4f95e4f40722dee29c033e0aaa0c1ebf42059772
Kamil Kowalewski <szamil24@gmail.com>
2021-01-15 19:14:26 +0100

    Delete launchSettings.json + setup JWT

--- EventoApi/Startup.cs
+++ EventoApi/Startup.cs
@@ -1,32 +1,40 @@
+using System.Text;
 using EventoCore.Repositories;
 using EventoInfrastructure.Mappers;
 using EventoInfrastructure.Repositories;
 using EventoInfrastructure.Services.Events;
 using EventoInfrastructure.Services.Users;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace EventoApi {
 
     public class Startup {
 
         /*------------------------ FIELDS REGION ------------------------*/
+        private readonly string applicationUrl;
+        private readonly string securityKey;
+
         public IConfiguration Configuration { get; }
 
         /*------------------------ METHODS REGION ------------------------*/
         public Startup(IConfiguration configuration) {
             Configuration = configuration;
+            applicationUrl = Configuration.GetSection("ApplicationUrl").Value;
+            securityKey = Configuration.GetSection("SecurityKey").Value;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services) {
             SetupScopedServices(services);
             SetupSingletonServices(services);
-
-            services.AddControllers();
+            SetupAuthentication(services);
+            SetupOthers(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,8 +45,10 @@ namespace EventoApi {
 
             app.UseHttpsRedirection();
             app.UseRouting();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.UseEndpoints(endpoints => endpoints.MapControllers());
+
         }
 
         private void SetupScopedServices(IServiceCollection services) {
@@ -50,6 +60,23 @@ namespace EventoApi {
 
         private void SetupSingletonServices(IServiceCollection services) {
             services.AddSingleton(AutoMapperConfig.Initialize());
+        }
+
+        private void SetupAuthentication(IServiceCollection services) {
+            services.AddAuthentication(option => {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer((option) => {
+                option.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateAudience = false,
+                    ValidIssuer = applicationUrl,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey))
+                };
+            });
+        }
+
+        private void SetupOthers(IServiceCollection services) {
+            services.AddControllers();
         }
 
     }
